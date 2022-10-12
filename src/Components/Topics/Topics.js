c13e2d55de7dff2eab8e5d19d696f85ff0658a0a
khorshed-alam-kbd <khorshedalam.kbd@gamil.com>
2022-10-12 22:41:10 +0600

    Header Added

--- src/Components/Topics/Topics.js
+++ src/Components/Topics/Topics.js
@@ -8,14 +8,21 @@ const Topics = () => {
     const quizTopics = Topics.data
     // console.log(quizTopics);
     return (
-        <div className='container mt-5 quiz-card'>
-            {
-                quizTopics.map(quizTopic => <Topic
-                    key={quizTopic.id}
-                    quizTopic={quizTopic}
-                ></Topic>)
-            }
+        <div className='container mt-5'>
+            <div className='header-bg rounded text-white'>
+                <h2 className='p-3 text-center'> Want to test your knowledge?</h2>
+                <p className='pt-0 p-3 text-center'> Brush up on your web developing knowledge <br /> with interesting questions.</p>
+            </div>
+            <div className='mt-5 quiz-card'>
+                {
+                    quizTopics.map(quizTopic => <Topic
+                        key={quizTopic.id}
+                        quizTopic={quizTopic}
+                    ></Topic>)
+                }
+            </div>
         </div>
+
     );
 };
 
